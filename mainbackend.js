f8620cfa130889a0811169be22b678ed0a0659dc
rherugu <rherugu@gmail.com>
2020-04-24 08:30:32 -0400

    bug fixes

---
+++ mainbackend.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const appjs = require("./src/backend/app");
+require("dotenv/config");
+
+app.use("/app", appjs);
+
+app.get("/", (req, res) => {
+  res.send("Powered by express © LocalMainStreet 2020");
+});
+
+app.listen(process.env.PORT || 3001, "0.0.0.0", () => {
+  console.log("mainbackend up and running");
+});
