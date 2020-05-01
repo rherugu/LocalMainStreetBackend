04c66563c2288944416681668cbe4f38268aead8
rherugu <rherugu@gmail.com>
2020-05-01 07:32:44 -0400

    Access-Control-Allow-Origin added

--- payment/server.js
+++ payment/server.js
@@ -12,6 +12,13 @@ app.use(compression()); //Compress all routes
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 
 app.get("/", (req, res) => {
   res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
