3b0e01ed27db3832c7b3fbd34cac9fe1ad35cadd
rherugu <rherugu@gmail.com>
2020-05-03 15:06:17 -0400

    Access COntrol allow origin

--- QRCode/main.js
+++ QRCode/main.js
@@ -6,6 +6,18 @@ const Main = require("./models/Main");
 
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, Authorization, X-Request-With"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
--- payment/server.js
+++ payment/server.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, Authorization, X-Request-With"
+  );
   next();
 });
 
