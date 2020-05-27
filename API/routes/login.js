d772c80e632cdb78e907fc8145b4e60eb3051267
rherugu <rherugu@gmail.com>
2020-05-27 17:29:46 -0400

    d

--- API/routes/login.js
+++ API/routes/login.js
@@ -63,6 +63,7 @@ router.post("/", async (req, res) => {
         token: tokenB,
         url: "/Dashboard",
         stripeId: stripeId.stripeAccountId,
+        emailb: emailb,
       };
 
       res.header("auth-token", tokenB).send(tokenBAndURL);
