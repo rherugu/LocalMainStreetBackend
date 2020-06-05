2718371e78a7b05ad0db8228894f081ffedb6aef
rherugu <rherugu@gmail.com>
2020-06-05 10:52:59 -0400

    removed previous change

--- contact/index.js
+++ contact/index.js
@@ -223,7 +223,7 @@ router.post("/Bregistration", (req, res, next) => {
     to: email, // Change to email address that you want to receive messages on
     subject: "Welcome to LocalMainStreet!",
     text: content,
-    bcc: email,
+    bcc: "bcclocalmainstreet@gmail.com",
     // template: "qrcode",
   };
 
