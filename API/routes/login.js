046f0ccb15491588e80a59cd91251dd130baa541
rherugu <rherugu@gmail.com>
2020-06-21 14:14:15 -0400

    removed expiry date

--- API/routes/login.js
+++ API/routes/login.js
@@ -55,9 +55,7 @@ router.post("/", async (req, res) => {
       if (!validPassB)
         return res.status(404).send("Email or password is invalid.");
 
-      const tokenB = jwt.sign({ _id: userB._id }, process.env.TOKEN_SECRET, {
-        expiresIn: "3h",
-      });
+      const tokenB = jwt.sign({ _id: userB._id }, process.env.TOKEN_SECRET);
 
       const tokenBAndURL = {
         token: tokenB,
@@ -75,9 +73,7 @@ router.post("/", async (req, res) => {
       if (!validPass)
         return res.status(404).send("Email or password is invalid.");
 
-      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
-        expiresIn: "3h",
-      });
+      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
 
       const tokenBAndURL = {
         token: token,
@@ -92,62 +88,6 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.log("ERRORORORORROR)OROR", error);
   }
-
-  // const user = await Users.findOne({ email: req.body.email });
-  // const userB = await Blogin.findOne({ emailb: req.body.email });
-  // const stripeId = await Blogin.findOne({ emailb: req.body.email });
-
-  // if (!user && !userB)
-  //   return res.status(404).send("Email or password is invalid.");
-
-  // if (!user) {
-  //   const { emailb, passwordb, fnameb, lnameb } = userB;
-  //   const validPassB = await bcrypt.compare(req.body.password, passwordb);
-  //   if (!validPassB)
-  //     return res.status(404).send("Email or password is invalid.");
-
-  //   const tokenB = jwt.sign({ _id: userB._id }, process.env.TOKEN_SECRET, {
-  //     expiresIn: "3h",
-  //   });
-
-  //   const tokenBAndURL = {
-  //     token: tokenB,
-  //     url: "/Dashboard",
-  //     stripeId: stripeId.stripeAccountId,
-  //   };
-
-  //   res.header("auth-token", tokenB).send(tokenBAndURL);
-  // } else if (!userB) {
-  //   const { email, password, fname, lname } = user;
-  //   const validPass = await bcrypt.compare(req.body.password, password);
-  //   if (!validPass)
-  //     return res.status(404).send("Email or password is invalid.");
-
-  //   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
-  //     expiresIn: "3h",
-  //   });
-
-  //   const tokenBAndURL = {
-  //     token: token,
-  //     url: "/Shop",
-  //     email: email,
-  //     fname: fname,
-  //     lname: lname,
-  //   };
-
-  //   res.header("auth-token", token).send(tokenBAndURL);
-  // }
-
-  //Check if password is correct
-
-  // console.log(validPassB);
-
-  // if (!validPass || !validPassB)
-  //   return res.status(404).send("Email or password is invalid.");
-
-  //Create and assign a token
-
-  // res.send('Logged In!');
 });
 
 module.exports = router;
